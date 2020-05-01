1e722a39b17cdfa175936697553589ada9d6cc47
Donnukrit Satirakul <kalima007c@gmail.com>
2020-05-02 00:03:18 +0700

    Normal

--- FinalExam/Main.java
+++ FinalExam/Main.java
@@ -1,6 +1,7 @@
 package FinalExam;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class Main {
     }
 }
 
-class Gui extends JFrame {
+class Gui extends JFrame implements ActionListener {
 
     private JPanel JP1 = new JPanel();
     private JPanel JP2 = new JPanel();
@@ -24,6 +25,7 @@ class Gui extends JFrame {
     private JTextArea Result = new JTextArea();
     
     Gui(){
+
         setTitle("Final Exam");
         // JP1 Start
             JP1.setLayout(new GridLayout(1,2));
@@ -34,8 +36,11 @@ class Gui extends JFrame {
         // JP2 Start
             JP2.setLayout(new FlowLayout(FlowLayout.CENTER));
             JP2.add(addButton);
+                addButton.addActionListener(this);
             JP2.add(subButton);
+                subButton.addActionListener(this);
             JP2.add(clearButton);
+                clearButton.addActionListener(this);
         // JP2 End
         //  JFrame SetLayout
             setLayout(new GridLayout(3,1));
@@ -45,5 +50,31 @@ class Gui extends JFrame {
         setVisible(true);
         setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public int addNumber(int x , int y){
+        return x + y;
+    }
+    public int subtract(int x, int y){
+        return x - y;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        int _Result = 0;
+        int a , b;
+            a = Integer.parseInt(ValueA.getText());
+            b = Integer.parseInt(ValueB.getText());
+        if(ae.getSource() == addButton) {
+            _Result = addNumber(a,b);
+            // System.out.println(Result);
+            Result.append(ValueA.getText() + " + " + ValueB.getText() + " = " + Integer.toString(_Result)+"\n");
+        }
+        if(ae.getSource() == subButton) {
+            _Result = subtract(a, b);
+            Result.append(ValueA.getText() + " - " + ValueB.getText() + " = " + Integer.toString(_Result)+"\n");
+        }
+        if(ae.getSource() == clearButton){
+            Result.setText("");
+        }
     }
 }
